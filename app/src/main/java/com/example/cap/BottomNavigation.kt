b0515e14c588a525eae1b8a592f02efb657f09a9
steph <stepha.guira@gmail.com>
2025-03-06 12:03:06 +0200

    bottom nav bar functionality

---
+++ app/src/main/java/com/example/cap/BottomNavigation.kt
@@ -0,0 +1,9 @@
+package com.example.cap
+import android.content.Context
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class BottomNavigation {
+    fun enableNavigation(context: Context, view: BottomNavigationView) {
+
+    }
+}
--- app/src/main/java/com/example/cap/MainActivity.kt
+++ app/src/main/java/com/example/cap/MainActivity.kt
@@ -7,16 +7,18 @@ import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.cap.databinding.ActivityMainBinding
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
-    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
     private lateinit var postAdapter: PostAdapter
+    private lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
@@ -25,36 +27,23 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
         getSupportActionBar()?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        bottomNav = findViewById(R.id.BottomNav) as BottomNavigationView
 
-        swipeRefreshLayout = findViewById(R.id.main)
-        swipeRefreshLayout.setOnRefreshListener(this)
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.ic_house -> replaceFragment(HomeFragment())
+                R.id.ic_profile -> replaceFragment(ProfileFragment())
+                R.id.ic_create -> replaceFragment(CreateFragment())
+                R.id.ic_search -> replaceFragment(SearchFragment()) }
+            true
+        }
 
-        val posts = listOf(
-            Post("dn", "Alice", "Hello, world!", "10:00 AM"),
-            Post("dn", "Bob", "This is my first tweet.", "10:15 AM"),
-            Post("dn", "Bob", "This is my first tweet.", "10:15 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Bob", "This is my first tweet.", "10:15 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM"),
-            Post("dn", "Charlie", "Kotlin is awesome!", "10:30 AM")
-        )
-
-        postAdapter = PostAdapter(posts)
-        recyclerView.adapter = postAdapter
     }
 
-    override fun onRefresh() {
-        Handler(Looper.getMainLooper()).postDelayed({
-            swipeRefreshLayout.isRefreshing = false
-        },300)
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
+
 }
